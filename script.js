abf06f56a8730d3472b25a1a1b76af8c448cdc22
MEPCENTER <mepcenter.as@gmail.com>
2025-07-13 03:25:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Website Loaded Successfully");
+document.addEventListener("DOMContentLoaded", function () {
+  const previewArea = document.getElementById("previewArea");
+  const previewImage = document.getElementById("previewImage");
+  const previewDesc = document.getElementById("previewDesc");
+
+  document.querySelectorAll(".thumb-box").forEach(box => {
+    box.addEventListener("click", function () {
+      const img = this.querySelector("img");
+      previewImage.src = img.dataset.full;
+      previewDesc.textContent = img.dataset.desc;
+      previewArea.classList.remove("d-none");
+      previewArea.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+});
